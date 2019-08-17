6c30c4f3f339cdac83d4c6a31e9ce05d258b2fde
Gabriel <rhernadnez028@west-mec.org>
2019-08-16 18:26:47 -0700

    Started started Thrid Excersice
    Created js file and added a function

---
+++ sub-unit-02-Enhancing-and-Validating-Forms/snoot.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/* 
+    Name: GAbriel Hernandez
+    Date: 8/16/19
+
+    snoot.js
+*/
+
+function removeSelectDefaults() {
+    var emptyBoxes = document.getElementsByTagName("select");
+    alert("select lists:" = emptyBoxes.length);
+}
